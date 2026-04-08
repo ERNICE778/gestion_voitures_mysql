ea023f5bb2ffb433e7444f88b396532750380f16
ERNICE778 <erniceken3@gmail.com>
2026-04-08 14:20:43 -0400

    ajout des instances pour la modification des informations d une voiture

--- main.py
+++ main.py
@@ -6,13 +6,31 @@ print("connexion reussie")
 conn.close()
     
 
-# v = Voiture("Toyota", "Corolla", 2020, 20000)
-# ajouter_voiture(v)
+v1 = Voiture("mercedes", "Corolla", 2020, 20000)
+v2 = Voiture("yaris", "Corolla", 2024, 20000)
 
-supprimer_voiture(1)
+ajouter_voiture(v1)
+ajouter_voiture(v2)
+
+
+
+
+
+
+
+supprimer_voiture(7)
+
 
 
 voitures = recuperer_voitures()
 
 for v in voitures:
     v.afficher_voiture()
+
+# modifier les infos d une voiture 
+v2.marque = "jetour"
+v2.modele ="evolution 2045"
+v2.annee=2025
+v2.prix=20200
+v2.id=4
+modifier_voiture(v2)
