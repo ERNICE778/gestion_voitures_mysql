ba2cd41028d623a70720a149b1854db8ccda99a0
ERNICE778 <erniceken3@gmail.com>
2026-04-08 13:37:32 -0400

    creation de des instances d une voiture

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from Voiture import Voiture
-from crud_db import connecter_db
+from crud_db import *
 
 conn = connecter_db()
 print("connexion reussie")
 conn.close()
     
+
+v = Voiture("Toyota", "Corolla", 2020, 20000)
+ajouter_voiture(v)
